9c706424e6835b6a8c583bc9949e999892595989
root <root@ip-172-31-7-82.ap-south-1.compute.internal>
2023-10-05 12:37:14 +0000

    updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello,Welcome to Naveen  React App!</h1>
+      <p>Welcome to my modified React application.</p>
+    </div>
+  );
+}
+
+export default App;
